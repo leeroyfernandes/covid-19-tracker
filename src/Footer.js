fe3f1aa7d73ff26f5ff5fd42a982859a6e98add2
leeroy fernandes <leeroyfernandes12@gmail.com>
2020-08-20 14:09:40 +0530

    First Commit

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import"./Footer.css"
+
+function Footer() {
+    return (
+        <div>
+            <footer className="app__footer">
+                <div className="container">
+                    <div classname="row">
+                        <p classname="ending">
+                            <hr/>
+                            &copy;2020 | Designed by Leeroy Fernandes
+                        </p>
+                    </div>
+
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
